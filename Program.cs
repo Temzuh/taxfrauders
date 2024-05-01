bd4847b850c781ace0baa93b006f49aefd044365
Teemu Kukkonen <teme.kukkonen@gmail.com>
2024-05-01 20:59:40 +0300

    use docker network

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-var connectionString = "server=localhost;user=root;password=taxfrauders;database=taxfrauders;port=6650";
+var connectionString = "server=172.17.0.1;user=root;password=taxfrauders;database=taxfrauders;port=6650";
 // Replace with your server version and type.
 // Use 'MariaDbServerVersion' for MariaDB.
 // Alternatively, use 'ServerVersion.AutoDetect(connectionString)'.
